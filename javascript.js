2d15911af72cdec40bf7aae762d6906be67cc447
santoshchappidi11 <santoshchappidi3@gmail.com>
2023-06-28 18:59:59 +0530

    added more, connect anddraft

--- javascript.js
+++ javascript.js
@@ -2,10 +2,15 @@ window.onload = () => {
   let open = document.getElementById("verified");
   let close = document.getElementById("close");
   let modalScreen = document.getElementById("modal-screen");
+  let moreButton = document.getElementById("more-button");
+  let moreModalLeft = document.getElementById("more-modal");
+  let draftButton = document.getElementById("draft-button");
+  let draftsScreen = document.getElementById("draft-screen");
+  let draftCloseButton = document.getElementById("draft-close");
 
   // ------------------------------------------------------------>
   open.addEventListener("click", function openModal() {
-    console.log('verified')
+    console.log("verified");
     modalScreen.style.display = "block";
   });
 
@@ -13,6 +18,22 @@ window.onload = () => {
     modalScreen.style.display = "none";
   });
 
+  moreButton.addEventListener("click", function openMoreModal() {
+    moreModalLeft.style.display = "block";
+  });
+
+  draftButton.addEventListener("click", function openDraftModal() {
+    draftsScreen.style.display = "block";
+    moreModalLeft.style.display = "none";
+  });
+
+  draftCloseButton.addEventListener("click", function closeDraftModal() {
+    draftsScreen.style.display = "none";
+  });
+
   openModal();
   closeModal();
+  openMoreModal();
+  openDraftModal();
+  closeDraftModal();
 };
